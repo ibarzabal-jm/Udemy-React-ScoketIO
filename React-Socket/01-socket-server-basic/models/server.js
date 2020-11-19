e7465007357e987db922a92de9335daa7152d8b3
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-11-19 20:36:34 -0300

    Update server.js

--- React-Socket/01-socket-server-basic/models/server.js
+++ React-Socket/01-socket-server-basic/models/server.js
@@ -4,6 +4,8 @@ const http      = require('http');
 const socketio  = require('socket.io');
 const path      = require('path');
 const Sockets   = require('./sockets');
+const cors      = require('cors')
+
 
 class Server {
     
@@ -21,6 +23,8 @@ class Server {
     middlewares() {
         // Desplegar el directorio publico
         this.app.use( express.static( path.resolve(__dirname,'../public' )  ) )
+        // Cors
+        this.app.use( cors() );
     }
 
     configurarSockets() {
