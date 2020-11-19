52c49194484938572d854b6d1a0fbb3f404ddc74
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-11-19 19:52:38 -0300

    Update index.js

--- React-Socket/01-socket-server-basic/index.js
+++ React-Socket/01-socket-server-basic/index.js
@@ -1,35 +1,9 @@
-// Servidor de Express
-const express = require('express');
-const app = express();
+const Server = require('./models/server');
+require('dotenv').config();
 
-// Servidor de sockets
-const server = require('http').createServer(app);
+const server = new Server();
 
-//Configuración del socket server
-const io = require('socket.io')(server);
+server.execute();
 
-// Desplegar el directorio publico
-app.use( express.static( __dirname + '/public') )
 
-// client o socket
-io.on('connection', ( socket ) => { 
-    
-    // socket.emit('mensaje-bienvenida', {
-    //     msg: 'Bienvenido al server',
-    //     fecha: new Date()
-    // });
 
-    // socket.on('Respuesta', (data)=>{
-    //     console.log(data);
-    // })
-    socket.on('mensaje-to-server', (data)=>{
-
-        // socket.emit('mensaje-from-server', data);
-        // io muestra a todos los que estan aqui
-        io.emit('mensaje-from-server', data);
-    })
- });
-
-server.listen(8080, ()=> {
-    console.log('Server corriendo en el puerto 3000')
-});
