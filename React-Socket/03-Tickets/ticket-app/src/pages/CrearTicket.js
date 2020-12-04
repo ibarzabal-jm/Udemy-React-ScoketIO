8c2ca11a9bf68124f356d0ffb7cf4a3faf93331f
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-12-03 23:54:49 -0300

    Create CrearTicket.js

---
+++ React-Socket/03-Tickets/ticket-app/src/pages/CrearTicket.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Button, Col, Row, Typography } from 'antd'
+import { DownloadOutlined } from '@ant-design/icons';
+import { useHideMenu } from '../hooks/useHideMenu';
+
+const { Title, Text} = Typography;
+
+export const CrearTicket = () => {
+
+    useHideMenu(true);
+
+    const nuevoTicket= () => {
+        console.log('nuevoticket')
+    }
+
+    return (
+        <>
+            <Row>
+                <Col span={14} offset={6} align="center">
+                    <Title level={3}>
+                        Presione el botón para generar un nuevo ticket
+                    </Title>
+
+                    <Button
+                        type="primary"
+                        shape="round"
+                        icon={ <DownloadOutlined />}
+                        size="large"
+                        onClick={ nuevoTicket }
+                    >
+                        Nuevo Ticket
+                    </Button>
+                </Col>
+            </Row>
+
+            <Row style={{marginTop:100}}>
+                <Col span={14 } offset={6} align="center">
+                    <Text level={2}>
+                        Sú numero
+                    </Text>
+                    <br/>
+                    <Text type="success" style={{ fontSize: 55}} >
+                        44
+                    </Text>
+                </Col>
+            </Row>
+        </>
+    )
+}
