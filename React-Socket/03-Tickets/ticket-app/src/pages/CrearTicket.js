55664d37ddd6614c1d5039377caf4cf43a46c1e3
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-12-05 17:09:30 -0300

    Update CrearTicket.js

--- React-Socket/03-Tickets/ticket-app/src/pages/CrearTicket.js
+++ React-Socket/03-Tickets/ticket-app/src/pages/CrearTicket.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { Button, Col, Row, Typography } from 'antd'
 import { DownloadOutlined } from '@ant-design/icons';
 import { useHideMenu } from '../hooks/useHideMenu';
+import { SocketContext } from '../context/SocketContext';
 
 const { Title, Text} = Typography;
 
@@ -9,8 +10,13 @@ export const CrearTicket = () => {
 
     useHideMenu(true);
 
+    const { socket } = useContext( SocketContext );
+    const [ticket, setTicket] = useState(null);
+
     const nuevoTicket= () => {
-        console.log('nuevoticket')
+        socket.emit('solicitar-ticket', null, (ticket)=> {
+            setTicket(ticket);
+        });
     }
 
     return (
@@ -33,17 +39,23 @@ export const CrearTicket = () => {
                 </Col>
             </Row>
 
-            <Row style={{marginTop:100}}>
-                <Col span={14 } offset={6} align="center">
-                    <Text level={2}>
-                        Sú numero
-                    </Text>
-                    <br/>
-                    <Text type="success" style={{ fontSize: 55}} >
-                        44
-                    </Text>
-                </Col>
-            </Row>
+            {
+                ticket && (
+                    <Row style={{marginTop:100}}>
+                        <Col span={14 } offset={6} align="center">
+                            <Text level={2}>
+                                Sú numero
+                            </Text>
+                            <br/>
+                            <Text type="success" style={{ fontSize: 55}} >
+                                
+                                { ticket.numero }
+                            </Text>
+                        </Col>
+                    </Row>
+                )
+            }
         </>
+
     )
 }
