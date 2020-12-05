ee891bffca0c462e0d337bda070a64d8e7db82f4
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-12-05 17:09:33 -0300

    Update Escritorio.js

--- React-Socket/03-Tickets/ticket-app/src/pages/Escritorio.js
+++ React-Socket/03-Tickets/ticket-app/src/pages/Escritorio.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Button, Col, Divider, Row, Typography } from 'antd'
 import { CloseCircleOutlined, RightCircleOutlined } from '@ant-design/icons';
 import { useHideMenu } from '../hooks/useHideMenu';
 import { getUsuarioStorage } from '../helpers/getUsuarioStorage';
 import { Redirect, useHistory } from 'react-router-dom';
+import { SocketContext } from '../context/SocketContext';
 
 const {Title, Text } = Typography;
 
@@ -12,16 +13,19 @@ export const Escritorio = () => {
     useHideMenu(false);
 
     const [usuario] = useState( getUsuarioStorage())
+    const { socket } = useContext( SocketContext )
+    const [ticket, setTicket] = useState(null)
     const history = useHistory();
 
     const salir = () =>{
         localStorage.removeItem('agente');
         localStorage.removeItem('escritorio');
-        history.replace('/ingresar');
-        
+        history.replace('/ingresar');    
     }
     const siguienteTicket = () =>{
-        console.log('siguienteTicket');
+        socket.emit('siguiente-ticket-escritorio', usuario, (ticket)=>{
+            setTicket(ticket);
+        } )
     }
 
     if ( !usuario.agente || !usuario.escritorio ){
@@ -50,16 +54,21 @@ export const Escritorio = () => {
 
             <Divider />
 
-            <Row>
-                <Col>
-                    <Text>Esta atendiendo el ticket número:</Text>
-                    <Text style={{ fontSize: 30}}
-                        type="danger"
-                    >
-                        55
-                    </Text>
-                </Col>
-            </Row>
+            {
+                ticket && (
+                    <Row>
+                        <Col>
+                            <Text>Esta atendiendo el ticket número:</Text>
+                            <Text style={{ fontSize: 30}}
+                                type="danger"
+                            >
+                                {ticket.numero}
+                            </Text>
+                        </Col>
+                    </Row>
+                )
+            }
+
 
             <Row>
                 <Col offset={ 18 } span={6} align="right" >
